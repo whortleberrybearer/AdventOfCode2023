fc0d83b6dcd8a4a53c66597bd0233f10577c5901
Barry Wheeler <wheelerbarry@hotmail.com>
2023-12-05 22:14:25 +0000

    Part 2

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,6 +1,7 @@
 ﻿var input = File.ReadAllText("Input.txt");
 var lines = input.Split("\r\n");
 var total = 0;
+var totalGearRatio = 0;
 
 for (int lineIndex = 0; lineIndex < lines.Length; lineIndex++)
 {
@@ -13,6 +14,7 @@ for (int lineIndex = 0; lineIndex < lines.Length; lineIndex++)
 
     for (int characterIndex = 0; characterIndex < line.Length; characterIndex++)
     {
+        // Part 1 needs numbers adjacent to symbols.
         if (char.IsNumber(line[characterIndex]))
         {
             if (numberStartIndex == -1)
@@ -53,9 +55,78 @@ for (int lineIndex = 0; lineIndex < lines.Length; lineIndex++)
             numberStartIndex = -1;
             numberEndIndex = -1;
         }
+
+        // Part 2 needs 2 numbers adjacent to a *.
+        if (line[characterIndex] == '*')
+        {
+            var numbers = new List<int>();
+
+            if (lineIndex > 0)
+            {
+                numbers.AddRange(FindNumbers(lines[lineIndex - 1], characterIndex));
+            }
+
+            numbers.AddRange(FindNumbers(lines[lineIndex], characterIndex));
+
+            if (lineIndex < lines.Length - 1)
+            {
+                numbers.AddRange(FindNumbers(lines[lineIndex + 1], characterIndex));
+            }
+
+            if (numbers.Count == 2)
+            {
+                var gearRatio = numbers[0] * numbers[1];
+
+                Console.WriteLine($"Gear ratio: {numbers[0]} * {numbers[1]} = {gearRatio}");
+
+                totalGearRatio += gearRatio;
+            }
+        }
     }
 
     Console.WriteLine();
 }
 
 Console.WriteLine($"Total: {total}");
+Console.WriteLine($"Total gear ration: {totalGearRatio}");
+
+IEnumerable<int> FindNumbers(string line, int multiplierIndex)
+{
+    var numbers = new List<int>();
+    var numberStartIndex = -1;
+    var numberEndIndex = -1;
+    var left = Math.Max(0, multiplierIndex - 1);
+    var rigth = Math.Min(line.Length - 1, multiplierIndex + 1);
+
+    for (int characterIndex = 0; characterIndex < line.Length; characterIndex++)
+    {
+        // Part 1 needs numbers adjacent to symbols.
+        if (char.IsNumber(line[characterIndex]))
+        {
+            if (numberStartIndex == -1)
+            {
+                numberStartIndex = characterIndex;
+            }
+
+            numberEndIndex = characterIndex;
+        }
+
+        if (((numberEndIndex != -1) && (numberEndIndex != characterIndex)) || (numberEndIndex == line.Length - 1))
+        {
+            // Found the end of a number
+            var number = int.Parse(line.Substring(numberStartIndex, numberEndIndex - numberStartIndex + 1));
+         
+            // Now check if that number is within the bounds of the multiplier.
+            if ((numberStartIndex <= rigth) && (numberEndIndex >= left))
+            {
+                numbers.Add(number);
+            }
+
+            // Need to reset the index for the next number.
+            numberStartIndex = -1;
+            numberEndIndex = -1;
+        }
+    }
+
+    return numbers;
+}
