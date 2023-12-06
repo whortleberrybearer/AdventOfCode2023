82f739a011f9bd83e762a58f5eb7a9bef36cadb3
whortleberrybearer <30476438+whortleberrybearer@users.noreply.github.com>
2023-12-06 08:58:01 +0000

    Day 4 (#4)

---
+++ Day4/Program.cs
@@ -0,0 +1,36 @@
+﻿var input = File.ReadAllText("Input.txt");
+var lines = input.Split("\r\n");
+var total = 0;
+var numberOfCards = Enumerable.Repeat(1, lines.Length).ToArray();
+
+for (int lineIndex = 0; lineIndex < lines.Length; lineIndex++)
+{
+    var line = lines[lineIndex];
+    
+    Console.WriteLine($"Line: {line}");
+
+    var parts = line.Split(':', '|');
+    var winningNumbers = parts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    var myNumbers = parts[2].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    var matchedNubers = winningNumbers.Intersect(myNumbers);
+
+    Console.WriteLine($"Matched Numbers: {string.Join(' ', matchedNubers)}");
+
+    if (matchedNubers.Count() == 1)
+    {
+        total += 1;
+    }
+    else
+    {
+        total += (int)Math.Pow(2, matchedNubers.Count() - 1);
+    }
+
+    // Increase the cards below this card 1 for each instance of this card we have.
+    for (int i = 0; i < matchedNubers.Count(); i++)
+    {
+        numberOfCards[lineIndex + 1 + i] = numberOfCards[lineIndex + 1 + i] + numberOfCards[lineIndex];
+    }
+}
+
+Console.WriteLine($"Total: {total}");
+Console.WriteLine($"Total cards: {numberOfCards.Sum(c => c)}");
