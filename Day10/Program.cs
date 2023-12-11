ac7b9dca48ffc287679fb5ae950755d1ffce6eea
Barry Wheeler <wheelerbarry@hotmail.com>
2023-12-11 12:44:15 +0000

    Part2

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -1,5 +1,6 @@
 ﻿var input = File.ReadAllLines("Input.txt");
 var maxtix = new char[input.Length][];
+var route = new char[maxtix.Length * 3][];
 int startX = 0;
 int startY = 0;
 
@@ -19,6 +20,45 @@ for (var lineIndex = 0; lineIndex < input.Length; lineIndex++)
         startY = lineIndex;
 
         Console.WriteLine($"StartX: {startX}, StartY: {startY}");
+    }
+
+    // Add the gaps to the route matrix as will be needing later.  // Anything on the edge can not be enclosed.
+    var routeIndex = lineIndex * 3;
+
+    if (lineIndex == 0)
+    {
+        route[routeIndex] = Enumerable.Repeat('0', line.Length * 3).ToArray();
+        route[routeIndex + 1] = Enumerable.Repeat('.', line.Length * 3).ToArray();
+        route[routeIndex + 2] = Enumerable.Repeat('.', line.Length * 3).ToArray();
+        route[routeIndex + 1][0] = '0';
+        route[routeIndex + 2][0] = '0';
+        route[routeIndex + 1][(line.Length * 3) - 1] = '0';
+        route[routeIndex + 2][(line.Length * 3) - 1] = '0';
+    }
+    else if (lineIndex == input.Length - 1)
+    {
+        route[routeIndex] = Enumerable.Repeat('.', line.Length * 3).ToArray();
+        route[routeIndex + 1] = Enumerable.Repeat('.', line.Length * 3).ToArray();
+        route[routeIndex + 2] = Enumerable.Repeat('0', line.Length * 3).ToArray();
+        route[routeIndex][0] = '0';
+        route[routeIndex + 1][0] = '0';
+        route[routeIndex][(line.Length * 3) - 1] = '0';
+        route[routeIndex + 1][(line.Length * 3) - 1] = '0';
+    }
+    else
+    {
+        // Random pipes can be enclosed, so set all to '.';
+        route[routeIndex] = Enumerable.Repeat('.', line.Length * 3).ToArray();
+        route[routeIndex + 1] = Enumerable.Repeat('.', line.Length * 3).ToArray();
+        route[routeIndex + 2] = Enumerable.Repeat('.', line.Length * 3).ToArray();
+
+        // The edges also can not be enclosed.
+        route[routeIndex][0] = '0';
+        route[routeIndex + 1][0] = '0';
+        route[routeIndex + 2][0] = '0';
+        route[routeIndex][(line.Length * 3) - 1] = '0';
+        route[routeIndex + 1][(line.Length * 3) - 1] = '0';
+        route[routeIndex + 2][(line.Length * 3) - 1] = '0';
     }
 }
 
@@ -50,12 +90,44 @@ do
     }
 
     currentPipe = maxtix[currentY][currentX];
+    route[(currentY * 3) + 1][(currentX * 3) + 1] = currentPipe;
+
+    if (currentPipe == '-')
+    {
+        route[(currentY * 3) + 1][currentX * 3] = '-';
+        route[(currentY * 3) + 1][(currentX * 3) + 2] = '-';
+    }
+    else if (currentPipe == '|')
+    {
+        route[currentY * 3][(currentX * 3) + 1] = '|';
+        route[(currentY * 3) + 2][(currentX * 3) + 1] = '|';
+    }
+    else if (currentPipe == '7')
+    {
+        route[(currentY * 3) + 1][currentX * 3] = '-';
+        route[(currentY * 3) + 2][(currentX * 3) + 1] = '|';
+    }
+    else if (currentPipe == 'J')
+    {
+        route[currentY * 3][(currentX * 3) + 1] = '|';
+        route[(currentY * 3) + 1][currentX * 3] = '-';
+    }
+    else if (currentPipe == 'L')
+    {
+        route[currentY * 3][(currentX * 3) + 1] = '|';
+        route[(currentY * 3) + 1][(currentX * 3) + 2] = '-';
+    }
+    else if (currentPipe == 'F')
+    {
+        route[(currentY * 3) + 1][(currentX * 3) + 2] = '-';
+        route[(currentY * 3) + 2][(currentX * 3) + 1] = '|';
+    }
 
     if (currentPipe != 'S')
     {
         currentDirection = FindCurrentDirection(currentPipe, currentDirection);
     }
-
+        
     Console.WriteLine($"Current pipe: {currentPipe}, Direction: {currentDirection}");
 }
 while (currentPipe != 'S');
@@ -65,6 +137,160 @@ Console.WriteLine($"Steps: {steps}");
 var furthest = steps / 2;
 
 Console.WriteLine($"Furthest: {furthest}");
+
+// Replace the 'S' with a pipe.
+
+if (route[(startY * 3) + 1][(startX * 3) + 3] == '-')
+{
+    route[(startY * 3) + 1][(startX * 3) + 2] = '-'; 
+}
+
+if (route[(startY * 3) + 1][(startX * 3) - 1] == '-')
+{
+    route[(startY * 3) + 1][(startX * 3)] = '-';
+}
+
+if (route[(startY * 3) + 3][(startX * 3) + 1] == '|')
+{
+    route[(startY * 3) + 2][(startX * 3) + 1] = '|';
+}
+
+if (startY > 0)
+{
+    if (route[(startY * 3) - 1][(startX * 3) + 1] == '|')
+    {
+        route[(startY * 3)][(startX * 3) + 1] = '|';
+    }
+}
+
+
+// Mark anything not enclosed;
+var replaced = false;
+
+do
+{
+    replaced = false;
+
+    for (var y = 1; y < route.Length - 1; y++)
+    {
+        for (var x = 1; x < route[y].Length - 1; x++)
+        {
+            if (route[y][x] == '.')
+            {
+                // If next to a 0, not enclosed.
+                for (var i = y - 1; i <= y + 1; i++)
+                {
+                    for (var j = x - 1; j <= x + 1; j++)
+                    {
+                        if (route[i][j] == '0')
+                        {
+                            route[y][x] = '0';
+
+                            replaced = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+while (replaced);
+
+// Now replace middle '.' is not enclosed.
+for (var y = 1; y < route.Length; y += 3)
+{
+    for (var x = 1; x < route[y].Length; x += 3)
+    {
+        // If sounded by '.', its enclosed.
+        var sounding = new char[]
+        {
+            route[y - 1][x - 1],
+            route[y - 1][x],
+            route[y - 1][x + 1],
+            route[y][x - 1],
+            route[y][x + 1],
+            route[y + 1][x - 1],
+            route[y + 1][x],
+            route[y + 1][x + 1]
+        };
+
+        if (sounding.All(c => c == '.'))
+        {
+            route[y][x] = 'I';
+        }
+    }
+}
+
+for (var i = 0; i < route.Length; i++)
+{
+    Console.WriteLine(string.Join("", route[i]));
+}
+
+var enclosed = route.Sum(l => l.Count(c => c == 'I'));
+
+Console.WriteLine($"Enclosed: {enclosed}");
+
+/*
+// Cleanup duplicate route.
+for (var y = 0; y < route.Length - 1; y += 2)
+{
+    for (var x = 0; x < route[y].Length - 1; x += 2)
+    {
+        if (route[y][x] == '7')
+        {
+            route[y + 1][x] = '.';
+            route[y][x] = '-';
+            route[y + 1][x + 1] = '|';
+        }
+        else if (route[y][x] == 'J')
+        {
+            route[y][x] = '.';
+            route[y][x + 1] = '|';
+            route[y + 1][x] = '-';
+        }
+        else if (route[y][x] == 'L')
+        {
+            route[y][x] = '|';
+            route[y][x + 1] = '.';
+            route[y + 1][x + 1] = '-';
+        }
+        else if (route[y][x] == 'F')
+        {
+            route[y][x + 1] = '-';
+            route[y + 1][x] = '|';
+            route[y + 1][x + 1] = '.';
+        }
+    }
+}
+
+// Replace any '-' or '|' bounded by a '.'.
+for (var y = 0; y < route.Length - 1; y++)
+{
+    for (var x = 0; x < route[y].Length - 1; x++)
+    {
+        if ((route[y][x] == '.') && (route[y][x + 1] == '-'))
+        {
+            route[y][x + 1] = '.';
+        }
+    }
+}
+
+for (var x = 0; x < route[0].Length - 1; x++)
+{
+    for (var y = 0; y < route.Length - 1; y++)
+    {
+        if ((route[y][x] == '.') && (route[y + 1][x] == '|'))
+        {
+            route[y + 1][x] = '.';
+        }
+    }
+}
+
+for (var i = 0; i < route.Length; i++)
+{
+    Console.WriteLine(string.Join("", route[i]));
+}
+*/
 
 Direction FindStartDirection()
 {
@@ -121,43 +347,3 @@ enum Direction
     East,
     West,
 }
-
-
-
-/*
-char FindCurrentDirection()
-{
-    var north = maxtix[startY - 1][startX] is '|' or '7' or 'F';
-    var south = maxtix[startY + 1][startX] is '|' or 'J' or 'L';
-    var east = maxtix[startY][startX + 1] is '-' or 'J' or '7';
-    var west = maxtix[startY][startX - 1] is '-' or 'L' or 'F';
-
-    if (north && south)
-    {
-        return '|';
-    }
-    else if (east && west)
-    {
-        return '-';
-    }
-    else if (north && east) 
-    {
-        return 'L';
-    }
-    else if (north && west)
-    {
-        return 'J';
-    }
-    else if (south && east)
-    {
-        return 'F';
-    }
-    else if (south && west)
-    {
-        return '7';
-    }
-
-    // Should never happen.
-    return '.';
-}
-*/
