441c9794daa5d714c52b27249743c5f5a5951f5d
Barry Wheeler <30476438+whortleberrybearer@users.noreply.github.com>
2023-12-12 16:50:46 +0000

    Some part 2 playing

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -6,8 +6,14 @@ foreach (var line in input)
     Console.WriteLine(line);
     
     var parts = line.Split(' ');
-    
-    rows.Add(new Row(parts[0], parts[1].Split(',').Select(c => int.Parse(c)).ToArray()));
+
+    var springs = parts[0];
+    var damaged = parts[1];
+
+    //springs = string.Join('?', Enumerable.Repeat(springs, 5));
+    //damaged = string.Join(',', Enumerable.Repeat(damaged, 5));
+
+    rows.Add(new Row(springs, damaged.Split(',').Select(c => int.Parse(c)).ToArray()));
 }
 
 var possibleCombinations = 0;
@@ -15,16 +21,18 @@ var possibleCombinations = 0;
 foreach (var row in rows)
 {
     var possibleLines = FindPossibleLines(row.Springs, row.Broken);
-
-    possibleCombinations += possibleLines.Count();
     
+    possibleCombinations += possibleLines.Count();
+
     Console.WriteLine($"Row: {row.Springs}, Possible: {possibleLines.Count()}");
+    Console.WriteLine();
 }
 
 Console.WriteLine($"Possible combinations: {possibleCombinations}");
 
 IEnumerable<string> FindPossibleLines(string springs, int[] broken)
 {
+    Console.WriteLine(springs);
     var possibleLines = new List<string>();
 
     if (MatchesPattern(springs, broken))
@@ -35,7 +43,8 @@ IEnumerable<string> FindPossibleLines(string springs, int[] broken)
     {
         var index = springs.IndexOf('?');
 
-        if (index > -1)
+        // Early escape clause.  If the formed blocks do not meet the patter so far, stop checking.
+        if ((index > -1) && PartialMatchesPattern(springs.Remove(index), broken))
         {
             var replaced = springs.ToCharArray();
             replaced[index] = '#';
@@ -58,6 +67,33 @@ bool MatchesPattern(string springs, int[] broken)
     if (groups.Length == broken.Length)
     {
         for (var i = 0; i < groups.Length; i++)
+        {
+            if (groups[i].Length != broken[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
+bool PartialMatchesPattern(string springs, int[] broken)
+{
+    var groups = springs.Split('.', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+    // If the last character isn't a '.', then its a partial group, so ignore it.
+    if (!springs.EndsWith(".") && (groups.Count > 0))
+    {
+        groups.RemoveAt(groups.Count - 1);
+    }
+
+    if (groups.Count <= broken.Length)
+    {
+        // Don't check the last group as it may not be fully formed.
+        for (var i = 0; i < groups.Count(); i++)
         {
             if (groups[i].Length != broken[i])
             {
