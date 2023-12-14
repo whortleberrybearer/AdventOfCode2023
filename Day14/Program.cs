0fb994dd25794d702261de592e1a0a4996b2c4e9
Barry Wheeler <30476438+whortleberrybearer@users.noreply.github.com>
2023-12-14 10:27:06 +0000

    Sample calculation

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -10,15 +10,26 @@ for (var i = 0; i < input.Count(); i++)
     panel[i] = line.ToCharArray();
 }
 
-for (var x = 0; x < panel[0].Length; x++)
+Console.WriteLine();
+
+TiltPanelNorth();
+TiltPanelWest();
+TiltPanelSouth();
+TiltPanelEast();
+
+TiltPanelNorth();
+TiltPanelWest();
+TiltPanelSouth();
+TiltPanelEast();
+
+TiltPanelNorth();
+TiltPanelWest();
+TiltPanelSouth();
+TiltPanelEast();
+
+foreach (var line in panel)
 {
-    for (var y = 1; y < panel.Length; y++)
-    {
-        if (panel[y][x] == 'O')
-        {
-            MoveUpIfAvailable(y, x);
-        }
-    }
+    Console.WriteLine(new string(line));
 }
 
 var totalLoad = 0;
@@ -36,7 +47,63 @@ for (var i = 0; i < panel.Length; i++)
 
 Console.WriteLine($"Total load: {totalLoad}");
 
-void MoveUpIfAvailable(int y, int x)
+void TiltPanelNorth()
+{
+    for (var x = 0; x < panel[0].Length; x++)
+    {
+        for (var y = 0; y < panel.Length; y++)
+        {
+            if (panel[y][x] == 'O')
+            {
+                MoveNorthIfAvailable(y, x);
+            }
+        }
+    }
+}
+
+void TiltPanelEast()
+{
+    for (var x = panel[0].Length - 1; x >= 0; x--)
+    {
+        for (var y = 0; y < panel.Length; y++)
+        {
+            if (panel[y][x] == 'O')
+            {
+                MoveEastIfAvailable(y, x);
+            }
+        }
+    }
+}
+
+void TiltPanelSouth()
+{
+    for (var x = 0; x < panel[0].Length; x++)
+    {
+        for (var y = panel.Length - 1; y >= 0; y--)
+        {
+            if (panel[y][x] == 'O')
+            {
+                MoveSouthIfAvailable(y, x);
+            }
+        }
+    }
+}
+
+void TiltPanelWest()
+{
+    for (var x = 0; x < panel[0].Length; x++)
+    {
+        for (var y = 0; y < panel.Length; y++)
+        {
+            if (panel[y][x] == 'O')
+            {
+                MoveWestIfAvailable(y, x);
+            }
+        }
+    }
+}
+
+void MoveNorthIfAvailable(int y, int x)
 {
     if (y == 0)
     {
@@ -48,6 +115,55 @@ void MoveUpIfAvailable(int y, int x)
         panel[y - 1][x] = 'O';
         panel[y][x] = '.';
         
-        MoveUpIfAvailable(y - 1, x);
+        MoveNorthIfAvailable(y - 1, x);
+    }
+}
+
+void MoveEastIfAvailable(int y, int x)
+{
+    if (x == (panel.Length - 1))
+    {
+        return;        
+    }
+
+    if (panel[y][x + 1] == '.')
+    {
+        panel[y][x + 1] = 'O';
+        panel[y][x] = '.';
+        
+        MoveEastIfAvailable(y, x + 1);
+    }
+}
+
+void MoveSouthIfAvailable(int y, int x)
+{
+    if (y == (panel.Length - 1))
+    {
+        return;        
+    }
+
+    if (panel[y + 1][x] == '.')
+    {
+        panel[y + 1][x] = 'O';
+        panel[y][x] = '.';
+        
+        MoveSouthIfAvailable(y + 1, x);
+    }
+}
+
+
+void MoveWestIfAvailable(int y, int x)
+{
+    if (x == 0)
+    {
+        return;        
+    }
+
+    if (panel[y][x - 1] == '.')
+    {
+        panel[y][x - 1] = 'O';
+        panel[y][x] = '.';
+        
+        MoveWestIfAvailable(y, x - 1);
     }
 }
