0423e7059b3d212919c656cd98ea1c68b463ba6e
Barry Wheeler <30476438+whortleberrybearer@users.noreply.github.com>
2023-12-15 08:25:07 +0000

    Part1

---
+++ Day15/Program.cs
@@ -0,0 +1,33 @@
+﻿var input = File.ReadLines("Input.txt");
+var hashTotal = 0;
+foreach (var line in input)
+{
+    Console.WriteLine(line);
+
+    foreach (var part in line.Split(','))
+    {
+        var hash = CalculateHash(part);
+
+        Console.WriteLine($"Part: {part}, Hash: {hash}");
+
+        hashTotal += hash;
+    }
+}
+
+Console.WriteLine($"Hash total: {hashTotal}");
+
+int CalculateHash(string s)
+{
+    var hash = 0;
+
+    foreach (var c in s.ToCharArray())
+    {
+        var ascii = (int)c;
+
+        hash += ascii;
+        hash *= 17;
+        hash %= 256;
+    }
+
+    return hash;
+}
