683990012fdcfd0488b20c11714e2871e9b8b219
Barry Wheeler <30476438+whortleberrybearer@users.noreply.github.com>
2023-12-20 10:01:51 +0000

    Pulse calculation

--- Day20/Program.cs
+++ Day20/Program.cs
@@ -61,11 +61,42 @@ foreach (var line in input)
 Console.WriteLine();
 
 var broadcaster = modules["broadcaster"] as BroadcastModule;
+var pulsesSent = new List<PulsesSent>();
+var endStates = new List<string>();
+var repeatIndex = -1;
 
-broadcaster.PushButton();
+do
+{
+    var sent = broadcaster.PushButton();
+    var states = string.Concat(modules.Values.Select(m => m.State));
 
-Console.WriteLine();
+    repeatIndex = endStates.IndexOf(states);
 
+    if (repeatIndex == -1)
+    {
+        endStates.Add(states);   
+        pulsesSent.Add(sent);
+    }
+} 
+while (repeatIndex == -1 || pulsesSent.Count == 1000);
+
+var highSent = pulsesSent.Take(repeatIndex).Sum(s => s.High);
+var lowSent = pulsesSent.Take(repeatIndex).Sum(s => s.Low);
+
+var repeat = (pulsesSent.Count - repeatIndex);
+var multiplier = 1000 / repeat;
+var remainder = 1000 % repeat;
+
+highSent += pulsesSent.Skip(repeatIndex).Sum(s => s.High) * multiplier;
+lowSent += pulsesSent.Skip(repeatIndex).Sum(s => s.Low) * multiplier;
+
+highSent += pulsesSent.Skip(repeatIndex).Take(remainder).Sum(s => s.High);
+lowSent += pulsesSent.Skip(repeatIndex).Take(remainder).Sum(s => s.Low);
+
+var totalSent = highSent * lowSent;
+
+Console.WriteLine($"Pulses sent: {totalSent}");
+/*
 broadcaster.PushButton();
 
 Console.WriteLine();
@@ -77,10 +108,12 @@ Console.WriteLine();
 broadcaster.PushButton();
 
 var sss = 0;
-
+*/
 record Module(string Id)
 {
     public List<Module> Links { get; } = new List<Module>();
+
+    public virtual string State => string.Empty;
 
     public virtual IEnumerable<NextPulse> SendPulse(bool high, string source)
     {
@@ -91,8 +124,9 @@ record Module(string Id)
 
 record BroadcastModule(string Id) : Module(Id)
 {
-    public void PushButton()
+    public PulsesSent PushButton()
     {
+        var pulsesSent = new PulsesSent();
         var nextPulses = new List<NextPulse>()
         {
             new NextPulse(this, false, "button"),
@@ -105,14 +139,25 @@ record BroadcastModule(string Id) : Module(Id)
             
             foreach (var pulseToSend in pulsesToSend)
             {
-                var pulse = pulseToSend.High ? "high" : "low";
-                
-                Console.WriteLine($"{pulseToSend.Source} -{pulse}-> {pulseToSend.Module.Id}");
-                
+                if (pulseToSend.High)
+                {
+                    Console.WriteLine($"{pulseToSend.Source} -high-> {pulseToSend.Module.Id}");
+
+                    pulsesSent.High += 1;
+                }
+                else
+                {
+                    Console.WriteLine($"{pulseToSend.Source} -low-> {pulseToSend.Module.Id}");
+                    
+                    pulsesSent.Low += 1;
+                }
+
                 nextPulses.AddRange(pulseToSend.Module.SendPulse(pulseToSend.High, pulseToSend.Source));
             }
         } 
         while (nextPulses.Any());
+
+        return pulsesSent;
     }
 
     public override IEnumerable<NextPulse> SendPulse(bool high, string source)
@@ -131,6 +176,8 @@ record BroadcastModule(string Id) : Module(Id)
 record FlipFlopModule(string Id) : Module(Id)
 {
     public bool On { get; private set; } = false;
+    
+    public override string State => On ? "1" : "0";
 
     public override IEnumerable<NextPulse> SendPulse(bool high, string source)
     {
@@ -155,6 +202,8 @@ record ConjunctionModule(string Id) : Module(Id)
 {
     public Dictionary<string, bool> ConnectedStates { get; set; } = new Dictionary<string, bool>();
 
+    public override string State => string.Concat(ConnectedStates.Values.Select(v => v ? "1" : "0"));
+    
     public override IEnumerable<NextPulse> SendPulse(bool high, string source)
     {
         var nextPulses = new List<NextPulse>();
@@ -174,3 +223,9 @@ record ConjunctionModule(string Id) : Module(Id)
 }
 
 record NextPulse(Module Module, bool High, string Source);
+
+record PulsesSent
+{
+    public int High { get; set; } = 0;
+    public int Low { get; set; } = 0;
+};
