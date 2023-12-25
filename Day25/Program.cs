5b06f4b6b034af853699c70c273c14cadcc9045f
Barry Wheeler <wheelerbarry@hotmail.com>
2023-12-25 07:50:38 +0000

    Reading wires

---
+++ Day25/Program.cs
@@ -0,0 +1,30 @@
+﻿var input = File.ReadAllLines("Input.txt");
+var allNodes = new List<string>();
+
+foreach (var line in input)
+{
+    Console.WriteLine(line);
+
+    allNodes.AddRange(line.Split(new char[] { ':', ' ' }, StringSplitOptions.RemoveEmptyEntries));
+}
+
+var nodes = allNodes.Distinct().ToDictionary(n => n, n => new List<Link>());
+
+foreach (var line in input)
+{
+    var parts = line.Split(new char[] { ':', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+    foreach (var join in parts.Skip(1))
+    {
+        var wire = new Wire();
+
+        nodes[parts[0]].Add(new Link(parts[0], join, wire));
+        nodes[join].Add(new Link(join, parts[0], wire));
+    }
+}
+
+Console.WriteLine();
+
+record Link(string Start, string End, Wire Wire);
+
+record Wire(bool Cut = false);
