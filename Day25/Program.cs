21c12165d8be701998e5bd20e48e18a3b74c63b1
Barry Wheeler <wheelerbarry@hotmail.com>
2023-12-25 23:54:14 +0000

    Optimisation of calcs?

--- Day25/Program.cs
+++ Day25/Program.cs
@@ -18,73 +18,117 @@ foreach (var line in input)
     {
         var wire = new Wire(parts[0], join);
 
+        /*(if ((parts[0] == "jqt" && join == "nvd")
+            || (parts[0] == "cmg" && join == "bvb") 
+            || (parts[0] == "pzl" && join == "hfx")
+            )
+        {
+            wire.Cut = true;
+        }
+        */
+
         nodes[parts[0]].Add(wire);
         nodes[join].Add(wire);
     }
 }
 
 Console.WriteLine();
-var routes = new Dictionary<string, (IEnumerable<string> Nodes, IEnumerable<Wire> Wires)>();
 
-for (var i = 0; i < nodes.Count; i++)
+for (var i = 0; i < 3; i++)
 {
-    for (var j = i + 1; j < nodes.Count; j++)
+    var routes = new Dictionary<string, (IEnumerable<string> Nodes, IEnumerable<Wire> Wires)>();
+    
+    routes = CalcaulareNodeRoutes(routes);
+
+    var groups = routes.Values.SelectMany(r => r.Wires).GroupBy(w => w).OrderBy(g => g.Count()).ToArray();
+
+    foreach (var group in groups)
     {
-        if (routes.ContainsKey($"{nodes.Keys.ElementAt(i)}-{nodes.Keys.ElementAt(j)}"))
+        Console.WriteLine($"{group.Key} = {group.Count()}");
+    }
+
+    var wire = groups.Last().Key;
+
+    wire.Cut = true;
+
+    Console.WriteLine($"Cut wire {wire}");
+
+    // Remove any routes that need re-expanding.
+    foreach (var route in routes.ToArray())
+    {
+        if (route.Value.Wires.Contains(wire))
         {
-            continue;
+            routes.Remove(route.Key);
+
+            Console.WriteLine($"Need to re-expand {route.Key}");
         }
+    }    
 
-        var route = FindRoute(nodes.Keys.ElementAt(i), nodes.Keys.ElementAt(j));
+    Console.WriteLine();
+}
 
-        if (route != null)
+//CalculateSections(wire1, wire2, wire3);
+
+void CalculateSections(Wire wire1, Wire wire2, Wire wire3)
+{
+    throw new NotImplementedException();
+}
+
+Dictionary<string, (IEnumerable<string> Nodes, IEnumerable<Wire> Wires)> CalcaulareNodeRoutes(Dictionary<string, (IEnumerable<string> Nodes, IEnumerable<Wire> Wires)> routes)
+{
+    for (var i = 0; i < nodes.Count; i++)
+    {
+        for (var j = i + 1; j < nodes.Count; j++)
         {
-            Console.WriteLine($"{nodes.Keys.ElementAt(i)}->{nodes.Keys.ElementAt(j)} = {string.Join(" - ", route.Value.Wires.Select(p => $"{p.Start}-{p.End}"))}");
-
-
-            //routes.Add($"{nodes.Keys.ElementAt(i)}-{nodes.Keys.ElementAt(j)}", (route.Value.Nodes, route.Value.Wires));
-            //routes.Add($"{nodes.Keys.ElementAt(j)}-{nodes.Keys.ElementAt(i)}", (route.Value.Nodes.Reverse(), route.Value.Wires.Reverse()));
-
-            for (var i1 = 0; i1 < route.Value.Nodes.Count(); i1++)
+            if (routes.ContainsKey($"{nodes.Keys.ElementAt(i)}-{nodes.Keys.ElementAt(j)}"))
             {
-                for (var j1 = i1 + 1; j1 < route.Value.Nodes.Count(); j1++)
+                continue;
+            }
+
+            var route = FindRoute(nodes.Keys.ElementAt(i), nodes.Keys.ElementAt(j), routes);
+
+            if (route != null)
+            {
+                Console.WriteLine($"{nodes.Keys.ElementAt(i)}->{nodes.Keys.ElementAt(j)} = {string.Join(" - ", route.Value.Wires.Select(p => $"{p.Start}-{p.End}"))}");
+
+
+                //routes.Add($"{nodes.Keys.ElementAt(i)}-{nodes.Keys.ElementAt(j)}", (route.Value.Nodes, route.Value.Wires));
+                //routes.Add($"{nodes.Keys.ElementAt(j)}-{nodes.Keys.ElementAt(i)}", (route.Value.Nodes.Reverse(), route.Value.Wires.Reverse()));
+
+                for (var i1 = 0; i1 < route.Value.Nodes.Count(); i1++)
                 {
-                    var start = route.Value.Nodes.ElementAt(i1);
-                    var end = route.Value.Nodes.ElementAt(j1);
-
-                    if (!routes.ContainsKey($"{start}-{end}") &&
-                        !routes.ContainsKey($"{end}-{start}"))
+                    for (var j1 = i1 + 1; j1 < route.Value.Nodes.Count(); j1++)
                     {
-                        var takeCount = (j1 - i1) + 1;
-                        var routeNodes = route.Value.Nodes.Skip(i1).Take(takeCount);
-                        var routeWires = route.Value.Wires.Skip(i1).Take(takeCount - 1);
+                        var start = route.Value.Nodes.ElementAt(i1);
+                        var end = route.Value.Nodes.ElementAt(j1);
 
-                        routes.Add($"{start}-{end}", (routeNodes.ToArray(), routeWires.ToArray()));
-                        routes.Add($"{end}-{start}", (routeNodes.Reverse().ToArray(), routeWires.Reverse().ToArray()));
+                        if (!routes.ContainsKey($"{start}-{end}") &&
+                            !routes.ContainsKey($"{end}-{start}"))
+                        {
+                            var takeCount = (j1 - i1) + 1;
+                            var routeNodes = route.Value.Nodes.Skip(i1).Take(takeCount);
+                            var routeWires = route.Value.Wires.Skip(i1).Take(takeCount - 1);
+
+                            routes.Add($"{start}-{end}", (routeNodes.ToArray(), routeWires.ToArray()));
+                            routes.Add($"{end}-{start}", (routeNodes.Reverse().ToArray(), routeWires.Reverse().ToArray()));
+                        }
                     }
                 }
             }
-        }
-        else
-        {
-            // No path for some reason.
-            int xxx = 0;
+            else
+            {
+                // No path for some reason.
+                int xxx = 0;
+            }
         }
     }
+
+    Console.WriteLine();
+
+    return routes;
 }
 
-Console.WriteLine();
-
-var groups = routes.Values.SelectMany(r => r.Wires).GroupBy(w => w).OrderByDescending(g => g.Count());
-
-foreach(var group in groups)
-{
-    Console.WriteLine($"{group.Key} = {group.Count()}");
-}
-
-Console.WriteLine();
-
-(IEnumerable<string> Nodes, IEnumerable<Wire> Wires)? FindRoute(string start, string end)
+(IEnumerable<string> Nodes, IEnumerable<Wire> Wires)? FindRoute(string start, string end, Dictionary<string, (IEnumerable<string> Nodes, IEnumerable<Wire> Wires)> routes)
 {
     var expandingRoutes = new List<Route>()
     {
@@ -119,7 +163,7 @@ Console.WriteLine();
             return (previousNodes.Concat(path.Nodes.Skip(1)), previousWires.Concat(path.Wires));
         }
 
-        foreach (var link in nodes[route.Node])
+        foreach (var link in nodes[route.Node].Where(n => n.Cut == false))
         {
             if (!previousWires.Contains(link))
             {
@@ -139,4 +183,7 @@ Console.WriteLine();
 
 record Route(string Node, IEnumerable<string> PreviousNodes, Wire Wire, IEnumerable<Wire> PreviousWires);
 
-record Wire(string Start, string End, bool Cut = false);
+record Wire(string Start, string End)
+{
+    public bool Cut { get; set; }
+}
